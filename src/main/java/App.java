f9a60c55e90aab202b102880bb3d89d1c05278aa
Esti Shay and Trevor Gill <trevor@>
2017-08-16 15:07:17 -0700

    add styling, CSS, and the tour forms

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,53 @@
+import dao.Sql2oCampsiteDao;
+import dao.Sql2oTourDao;
+import models.Campsite;
+import models.Tour;
+import org.sql2o.Sql2o;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.staticFileLocation;
 
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        String connectionString = "jdbc:h2:~/biketours.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        Sql2oCampsiteDao campsiteDao = new Sql2oCampsiteDao(sql2o);
+        Sql2oTourDao tourDao = new Sql2oTourDao(sql2o);
+
+        //get: show all campsites in all categories and show all categories
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Tour> allTours = tourDao.getAll();
+            model.put("tours", allTours);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/tours/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Tour> allTours = tourDao.getAll();
+            model.put("tours", allTours);
+            return new ModelAndView(model, "tour-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/tours", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("newName");
+            int difficulty = Integer.parseInt(request.queryParams("newDifficulty"));
+            int rating = Integer.parseInt(request.queryParams("newRating"));
+            Tour newTour = new Tour(name,difficulty,rating);
+            tourDao.add(newTour);
+            response.redirect("/");
+            return null;
+        });
+    }
 
 }
