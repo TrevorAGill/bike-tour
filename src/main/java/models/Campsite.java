cab44aaf30360f86c72ea5639d6b6dcf9bd36a3a
Esti Shay and Trevor Gill <esti@>
2017-08-16 11:43:23 -0700

    commit work in progress before lunch

--- src/main/java/models/Campsite.java
+++ src/main/java/models/Campsite.java
@@ -2,4 +2,80 @@ package models;
 
 
 public class Campsite {
+    private String name;
+    private int rating;
+    private int cost;
+    private int id;
+    private int tourId;
+
+    public Campsite (String name, int rating, int cost, int tourId){
+        this.name = name;
+        this.rating = rating;
+        this.cost = cost;
+        this.tourId = tourId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Campsite campsite = (Campsite) o;
+
+        if (rating != campsite.rating) return false;
+        if (cost != campsite.cost) return false;
+        if (id != campsite.id) return false;
+        if (tourId != campsite.tourId) return false;
+        return name.equals(campsite.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + rating;
+        result = 31 * result + cost;
+        result = 31 * result + id;
+        result = 31 * result + tourId;
+        return result;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTourId() {
+        return tourId;
+    }
+
+    public void setTourId(int tourId) {
+        this.tourId = tourId;
+    }
 }
--- src/test/java/dao/Sql2oCampsiteDaoTest.java
+++ src/test/java/dao/Sql2oCampsiteDaoTest.java
@@ -1,8 +1,18 @@
+package dao;
+
+import org.junit.After;
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by Guest on 8/16/17.
- */
+
 public class Sql2oCampsiteDaoTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
 
 }
